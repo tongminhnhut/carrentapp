f39512b0d57aae21899db94b7aa976aa24663c66
tongminhnhut <tongminhnhut@gmail.com>
2024-08-25 18:02:06 +0700

    first commit

--- app/src/main/java/com/tongminhnhut/carrent/presentation/ui/theme/MainActivity.kt
+++ app/src/main/java/com/tongminhnhut/carrent/presentation/ui/theme/MainActivity.kt
@@ -38,11 +38,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarRentTheme {
-        Greeting("Android")
-    }
-}
